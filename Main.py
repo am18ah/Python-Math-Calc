7153c0fae670f7eef6c86f960a1ee0e829ab44a4
hollyxj <74798049+hollyxj@users.noreply.github.com>
2021-10-05 14:47:48 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
 #this will serve as the main page for the functionality
+
+# use Django to create web interface
+
+# function for basic math
+
+# function for pre-calculus
+
+# function for trigonometry
+
+# function for statistics
+
+# function for calculus
+
+# function for linear algebra
