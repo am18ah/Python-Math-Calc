a3b52d0147183bf36fa3358cc5190e4712b23d8c
KayleighElmo <kre18i@my.fsu.edu>
2021-11-03 15:30:01 -0400

    added login screen and a base html

--- django_project/math_app/urls.py
+++ django_project/math_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.home, name='math-home'),
     path('register/', views.register_request, name='math-register'),
     path('about/', views.about, name='math-about'),
-
-]
+    path('login/', views.login, name='math-login'),
+    
+    ]
--- django_project/math_app/views.py
+++ django_project/math_app/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import  render, redirect
 from .forms import NewUserForm
-from django.contrib.auth import login
 from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def home(request):
@@ -13,7 +13,24 @@ def home(request):
 def about(request):
     return render(request, 'math_app/about.html', {'title': 'About'})
 
-
+def login(request):
+    if request.method == "POST":
+            form = AuthenticationForm(request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password')
+                user = authenticate(username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    messages.info(request, f"You are now logged in as {username}.")
+                    return redirect("main:homepage")
+                else:
+                    messages.error(request,"Invalid username or password.")
+            else:
+                messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="math_app/login.html", context={"login_form":form})
+        #return render(request, 'math_app/login.html', {'title': 'Login'})
 
 def register_request(request):
     if request.method == "POST":
