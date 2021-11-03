5b2c52dfa5ec6a59687235b44f67f5855435da01
Andres Molina <am18ah@my.fsu.edu>
2021-11-02 21:00:13 -0400

    Made sample login info without a database
    html and some back end for a user registration

--- django_project/math_app/urls.py
+++ django_project/math_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='math-home'),
+    path('register/', views.register_request, name='math-register'),
     path('about/', views.about, name='math-about'),
+
 ]
--- django_project/math_app/views.py
+++ django_project/math_app/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
 
 def home(request):
     return render(request, 'math_app/home.html')
 
+
 def about(request):
     return render(request, 'math_app/about.html', {'title': 'About'})
+
+
+
+def register_request(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful." )
+            return redirect("main:homepage")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render (request=request, template_name="math_app/register.html", context={"register_form":form})
