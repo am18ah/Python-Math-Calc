56f8f4d4c989b361031d608622fd1fe7efa77e65
KayleighElmo <kre18i@my.fsu.edu>
2021-12-01 23:47:56 -0500

    Merged Matt with Kay

--- Test.py
+++ Test.py
@@ -2,6 +2,18 @@
 import Main as test
 import math
 
+L1 = [1, 2, 3, 4, 5]
+L2 = [1, 2, 4, 4, 5]
 print (test.Add(3, 'x'))
 print(test.findSin(math.pi)) 
-print(test.power(3,2))
+print(test.Power(3,2))
+
+print("Testing Trig: ")
+print(test.findSin(math.pi)) 
+print(test.findCos(0))
+
+print("Testing Stats: ")
+print(test.findMin(L1))
+print(test.findMedian(L1))
+print(test.findMode(L2))
+print(test.findRange(L1))
