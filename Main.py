729de000aa71f84c68ff4aefbedb64869c345a11
Matthew Torres <66329929+Krash-Nebula@users.noreply.github.com>
2021-12-02 07:32:41 -0500

    organized into class

--- Main.py
+++ Main.py
@@ -7,120 +7,110 @@ import numpy
 # use Django to create web interface
 
 # function for basic math
+class mathMethods:
+    def Add(x, y):
+            return x+y
 
-def Add(x, y):
-        return x+y
+    def Sub(x, y):
+            return x-y
 
-def Sub(x, y):
-        return x-y
+    def Multiply(x, y):
+            return x*y
 
-def Multiply(x, y):
-        return x*y
+    def Divide(x, y):
+            return x/y
 
-def Divide(x, y):
-        return x/y
+    def Power(x,y):
+            return x ** y
 
-def Power(x,y):
-        return x ** y
-
-def Root(x, y = 2):
-    if y == 2:
-        return math.sqrt(2)     #square root
-    else:
-        return x ** (1/y)       #root y
+    def Root(x, y = 2):
+        if y == 2:
+            return math.sqrt(2)     #square root
+        else:
+            return x ** (1/y)       #root y
 
 # function for pre-calculus
 
-def findLog(x, y = 10):
-    if y == 10:
-        return math.log10(x)        #more accurate than log(x, 10)
-    else:
-        return math.log(x, y)
-def findln(x):
-    return math.log(x)  #no base specification defaults to base e (ln)
+    def findLog(x, y = 10):
+        if y == 10:
+            return math.log10(x)        #more accurate than log(x, 10)
+        else:
+            return math.log(x, y)
+    def findln(x):
+        return math.log(x)  #no base specification defaults to base e (ln)
 
 # function for trigonometry
 
-def findSin(x, choice):
-    if choice == 'R':
-        ans = math.sin(x)
-    elif choice == 'D':
-        ans = math.sin(math.radians(x))    #if degrees is entered converts to radians to compute
-    if ans < 0.0000000000001:       #solves rounding error for expected 0 result like sin(pi)
-        ans = 0
-    return ans
+    def findSin(x, choice):
+        if choice == 'R':
+            ans = math.sin(x)
+        elif choice == 'D':
+            ans = math.sin(math.radians(x))    #if degrees is entered converts to radians to compute
+        if ans < 0.0000000000001:       #solves rounding error for expected 0 result like sin(pi)
+            ans = 0
+        return ans
 
-def findCos(x, choice):
-    if choice == 'R':
-        ans = math.cos(x)
-    elif choice == 'D':
-        ans = math.cos(math.radians(x))
-    if ans < 0.0000000000001:
-        ans = 0
-    return ans
+    def findCos(x, choice):
+        if choice == 'R':
+            ans = math.cos(x)
+        elif choice == 'D':
+            ans = math.cos(math.radians(x))
+        if ans < 0.0000000000001:
+            ans = 0
+        return ans
 
-def findTan(x, choice):
-    if choice == 'R':
-        ans = math.tan(x)
-    elif choice == 'D':
-        ans = math.tan(math.radians(x))
-    if ans < 0.0000000000001:
-        ans = 0
-    return ans
+    def findTan(x, choice):
+        if choice == 'R':
+            ans = math.tan(x)
+        elif choice == 'D':
+            ans = math.tan(math.radians(x))
+        if ans < 0.0000000000001:
+            ans = 0
+        return ans
 
-def findCsc(x, choice):
-    if choice == 'R':
-        ans = (1/math.sin(x))
-    elif choice == 'D':
-        ans = (1/math.sin(math.radians(x)))
-    if ans < 0.0000000000001:
-        ans = 0
-    return ans
+    def findCsc(x, choice):
+        if choice == 'R':
+            ans = (1/math.sin(x))
+        elif choice == 'D':
+            ans = (1/math.sin(math.radians(x)))
+        if ans < 0.0000000000001:
+            ans = 0
+        return ans
 
-def findSec(x, choice):
-    if choice == 'R':
-        ans = (1/math.cos(x))
-    elif choice == 'D':
-        ans = (1/math.cos(math.radians(x)))
-    if ans < 0.0000000000001:
-        ans = 0
-    return ans
+    def findSec(x, choice):
+        if choice == 'R':
+            ans = (1/math.cos(x))
+        elif choice == 'D':
+            ans = (1/math.cos(math.radians(x)))
+        if ans < 0.0000000000001:
+            ans = 0
+        return ans
 
-def getPi():
-    return numpy.pi
+    def getPi():
+        return numpy.pi
 
 # function for statistics
 
-def findMean(x):
-    return statistics.mean(x)
+    def findMean(x):
+        return statistics.mean(x)
 
-def findMedian(x):
-    return statistics.median(x)
+    def findMedian(x):
+        return statistics.median(x)
 
-def findMode(x):
-    return statistics.mode(x)
+    def findMode(x):
+        return statistics.mode(x)
 
-def findMin(x):
-    return numpy.min(x)
+    def findMin(x):
+        return numpy.min(x)
 
-def findMax(x):
-    return numpy.max(x)
+    def findMax(x):
+        return numpy.max(x)
 
-def findRange(x):
-    return numpy.max(x) - numpy.min(x)
+    def findRange(x):
+        return numpy.max(x) - numpy.min(x)
 
-def findStdDev(x):
-    statistics.stdev(x)
+    def findStdDev(x):
+        statistics.stdev(x)
 
-def findVar(x):
-    statistics.variance(x)
-
-# function for calculus
-def findIndefIntegral(x): #Indefinite Integral
-    pass
-
-def findDefIntegral(x): #Definite Integral
-    pass
-
-def findDerivative(x):
-    pass
+    def findVar(x):
+        statistics.variance(x)
--- Test.py
+++ Test.py
@@ -4,15 +4,15 @@ import math
 
 L1 = [1, 2, 3, 4, 5]
 L2 = [1, 2, 4, 4, 5] 
-print(test.Power(3,2))
+print(test.mathMethods.Power(3,2))
 
 print("Testing Trig: ")
-print(test.findSin(math.pi, 'R')) 
-print(test.findCos(0, 'R'))
-print(test.findCos(90, 'D'))
+print(test.mathMethods.findSin(math.pi, 'R')) 
+print(test.mathMethods.findCos(0, 'R'))
+print(test.mathMethods.findCos(90, 'D'))
 
 print("Testing Stats: ")
-print(test.findMin(L1))
-print(test.findMedian(L1))
-print(test.findMode(L2))
-print(test.findRange(L1))
+print(test.mathMethods.findMin(L1))
+print(test.mathMethods.findMedian(L1))
+print(test.mathMethods.findMode(L2))
+print(test.mathMethods.findRange(L1))
