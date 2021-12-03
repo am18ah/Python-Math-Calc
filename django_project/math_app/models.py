33e7e0dcf5dcd2990b3b04986fc50ee96e13beb0
KayleighElmo <kre18i@my.fsu.edu>
2021-12-03 18:47:03 -0500

    fixed error

--- django_project/math_app/models.py
+++ django_project/math_app/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.conif import settings
+
 
 class Equation(models.Model):
 	type_of_math = models.CharField(max_length=100)
 	math = models.TextField()
 	time_posted = models.TimeField(default=timezone.now)
-	author = models.ForeignKey(User, on_delete = models.CASCADE)
+	author = models.CharField(max_length=100, primary_key = True)
 
 	def __str__(self):
 		return self.type_of_math + " " + self.math + " " + self.author
